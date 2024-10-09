69e2ea4501a908682a6fd2b077392e5b84b9e1e4
Linus Almgren <linus.almgren@gmail.com>
2024-10-09 17:39:06 +0200

    add types to mysql server options

--- typescript/config/connectDB.ts
+++ typescript/config/connectDB.ts
@@ -1,12 +1,14 @@
-import mysql from 'mysql2';
+import mysql, { ConnectionOptions } from 'mysql2';
 
-const db = mysql.createConnection({
+const options: ConnectionOptions = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT) || 3306,
-});
+};
+
+const db = mysql.createConnection(options);
 
 db.connect((err) => {
   if (err) throw err;
