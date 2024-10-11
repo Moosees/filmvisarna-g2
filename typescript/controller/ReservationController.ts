0a5f6cd3a9d9f9347ae51837782752e138c3eed7
Linus Almgren <linus.almgren@gmail.com>
2024-10-12 01:39:05 +0200

    add reservation_ticket insert query to createNewReservation
    new createInsertTemplate function can also be used for res_seat_screen
    insert query

--- typescript/controller/ReservationController.ts
+++ typescript/controller/ReservationController.ts
@@ -4,12 +4,23 @@ import db from '../config/connectDB.js';
 export const createNewReservation = async () => {
   try {
     await db.beginTransaction();
-    await db.execute(
-      'CALL create_reservation(:email, :screeningId, @member_id);',
+    const result1 = await db.execute(
+      'CALL create_reservation(:email, :screeningId, @reservationId);',
       { email: 'node@test.com', screeningId: 5 }
     );
-    const result = await db.query('SELECT @member_id');
-    console.log(result);
+    console.log(result1);
+
+    const result2 = await db.execute(
+      `INSERT INTO reservation_ticket (reservation_id, ticket_id) VALUES ${createInsertTemplate(1, 2)};`,
+      [1, 2]
+    );
+    console.log(result2);
+
+    const result3 = await db.query(
+      'SELECT * FROM reservation_ticket rt WHERE rt.reservation_id = @reservationId'
+    );
+    console.log(result3);
+
     await db.rollback();
     await db.end();
 
@@ -18,4 +29,9 @@ export const createNewReservation = async () => {
     console.log(error);
     // res.status(500).json({ error });
   }
+};
+
+const createInsertTemplate = (cols: number, rows: number) => {
+  const row = '(@reservationId, ' + new Array(cols).fill('?').join(', ') + ')';
+  return new Array(rows).fill(row).join(', ');
 };
