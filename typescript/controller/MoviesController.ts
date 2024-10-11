9127e51c9b9c6be662c68d5c06760a0bb6b1912f
Johan Håkansson <johan1hakansson@gmail.com>
2024-10-11 11:09:50 +0200

    works and changed query to execute

--- typescript/controller/MoviesController.ts
+++ typescript/controller/MoviesController.ts
@@ -1,20 +1,26 @@
 import db from '../config/connectDB.js';
 import { Request, Response } from 'express';
+import { RowDataPacket } from 'mysql2';
 
 const getSpecificMovie = async (req: Request, res: Response) => {
   const { id } = req.params;
 
   try {
     const query: string = 'SELECT * FROM  movie m WHERE m.id =?';
-    db.query(query, [id], (error, results) => {
+
+    db.execute(query, [id], (error, results: RowDataPacket[]) => {
       if (error) {
         return res.status(500).json({ msg: 'Något gick fel', error });
       }
 
-      res.status(200).json(results);
+      if (results.length === 0) {
+        return res.status(404).json({ msg: 'Film inte hittad' });
+      }
+
+      if (results) res.status(200).json(results);
     });
   } catch (error) {
-    res.status(500).json({ msg: 'något gick fel', error });
+    res.status(500).json({ msg: 'Något gick fel', error });
   }
 };
 
