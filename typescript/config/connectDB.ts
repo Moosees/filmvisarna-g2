e939db75fa53b1b92db078198e55bc5a4050a91a
EmilyWahlin <emilysaletti@gmail.com>
2024-10-13 16:39:58 +0200

    added back the import, deleted unused code in connectdb

--- typescript/config/connectDB.ts
+++ typescript/config/connectDB.ts
@@ -1,21 +1,3 @@
-// import mysql, { ConnectionOptions } from 'mysql2';
-
-// const options: ConnectionOptions = {
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   port: Number(process.env.DB_PORT) || 3306,
-// };
-
-// const db = mysql.createConnection(options);
-
-// db.connect((err) => {
-//   if (err) throw err;
-//   console.log(`Connected to MySQL ${process.env.DB_NAME} database!`);
-// });
-
-// export default db;
 import mysql from 'mysql2/promise';
 
 const db = mysql.createPool({
