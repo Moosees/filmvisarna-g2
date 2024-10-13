6662f84204cf40f646910c6c1d91d01da687e517
Sara Johnsson <sara86persson@hotmail.com>
2024-10-13 14:39:03 +0200

    added options to sessionStore and added sessionStore to server.ts

--- typescript/helpers/sessionStore.ts
+++ typescript/helpers/sessionStore.ts
@@ -1,6 +1,18 @@
 import MySQLStore from 'express-mysql-session';
 import db from '../config/connectDB';
 
-export default function sessionStore(session) {
-  return new MySQLStore({}, db);
+export default function sessionStore(): MySQLStore {
+  const options = {
+    createDatabaseTable: true,
+    schema: {
+      tableName: 'sessions',
+      columnNames: {
+        session_id: 'session_id',
+        expires: 'expires',
+        data: 'data',
+      },
+    },
+  };
+
+  return new MySQLStore(options, db);
 }
--- typescript/server.ts
+++ typescript/server.ts
@@ -1,12 +1,27 @@
 import './config/dotenv.js';
 import express from 'express';
 import router from './routes.js';
+import session from 'express-session';
+import sessionStore from './helpers/sessionStore.js';
 
 const app = express();
 
 app.use(express.json());
 
 const PORT = process.env.SERVER_PORT || 3008;
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || 'yourFallbackSecret',
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore(),
+    cookie: {
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 1000 * 60 * 60,
+    },
+  })
+);
 
 app.use(router);
 
