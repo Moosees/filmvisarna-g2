d93b067ea2b939f636869272bd50e15b95cddbb3
Linus Almgren <linus.almgren@gmail.com>
2024-10-14 12:58:59 +0200

    move dbOptions to exported variable and add charset utf8mb to options

--- typescript/config/connectDB.ts
+++ typescript/config/connectDB.ts
@@ -1,13 +1,16 @@
-import mysql from 'mysql2/promise';
+import mysql, { PoolOptions } from 'mysql2/promise';
 
-const db = mysql.createPool({
+export const dbOptions: PoolOptions = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT) || 3306,
   namedPlaceholders: true,
-});
+  charset: 'utf8mb4',
+};
+
+const db = mysql.createPool(dbOptions);
 
 // Testa anslutningen
 db.getConnection()
