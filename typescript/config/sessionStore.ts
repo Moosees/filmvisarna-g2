b120e17445127e844312b1df7a69d66386bb6463
Linus Almgren <linus.almgren@gmail.com>
2024-10-15 05:16:28 +0200

    move all session code to config folder

---
+++ typescript/config/sessionStore.ts
@@ -0,0 +1,38 @@
+import MySQLStoreFactory from 'express-mysql-session';
+import * as session from 'express-session';
+import { dbOptions } from './connectDB.js';
+
+declare module 'express-session' {
+  interface SessionData {
+    user?: {
+      id: number;
+      email: string;
+      first_name: string;
+      last_name: string;
+      role: string;
+    };
+  }
+}
+
+const MySqlStore = MySQLStoreFactory(session);
+const store = new MySqlStore(dbOptions);
+
+store
+  .onReady()
+  .then(() => {
+    console.log('MySQLStore ready');
+  })
+  .catch((error: Error) => {
+    console.error(error);
+  });
+
+export default session.default({
+  secret: process.env.SESSION_SECRET || 'yourFallbackSecret',
+  resave: false,
+  saveUninitialized: false,
+  store,
+  cookie: {
+    secure: process.env.NODE_ENV === 'production',
+    maxAge: 1000 * 60 * 60,
+  },
+});
