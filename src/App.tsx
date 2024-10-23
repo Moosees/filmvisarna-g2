986860043f50ce198ecfd25ac6c61670149c771e
Linus Almgren <linus.almgren@gmail.com>
2024-10-23 11:58:09 +0200

    move MainHeading to App layout and update with each route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, UIMatch, useMatches } from 'react-router-dom';
 import Header from './components/header/Header';
+import MainHeading from './components/mainHeading/MainHeading';
+
+interface AppMatch extends UIMatch {
+  handle: {
+    title?: string;
+  };
+}
 
 function App() {
+  const matches = useMatches() as unknown[] as AppMatch[];
+  const titles = matches.filter(({ handle }) => handle?.title);
+
   return (
-    <div className="min-vh-100 d-flex flex-column gap-5">
+    <div className="min-vh-100 d-flex flex-column gap-3">
       <Header />
+      {titles.length > 0 && titles[0].handle.title && (
+        <MainHeading title={titles[0].handle.title} />
+      )}
       <main className="flex-grow-1 container">
         <Outlet />
       </main>
