65ee5a96ed3de1ca91043f1c51cd51bde8e51af0
Linus Almgren <linus.almgren@gmail.com>
2024-10-23 08:36:36 +0200

    use TestCard as HomePage and make a route for it

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-// import { Button } from 'react-bootstrap';
-
 import { Outlet } from 'react-router-dom';
 import Header from './components/header/Header';
-import TestCard from './components/movieCard/TestCard';
 
 function App() {
   return (
     <div className="min-vh-100 d-flex flex-column">
       <Header />
-      <main className="flex-grow-1">
-        <TestCard />
+      <main className="flex-grow-1 container">
+        <Outlet />
       </main>
-      <Outlet />
       <div>Footer</div>
     </div>
   );
--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
 import RegisterPage from './pages/registerpage/RegisterPage';
+import HomePage from './pages/homepage/HomePage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
       {
         path: '/registrera',
         element: <RegisterPage />,
