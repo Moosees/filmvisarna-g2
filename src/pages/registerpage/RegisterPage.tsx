feba3d2b076b30481e5f190c352c7d719205b9a9
Sara Johnsson <sara86persson@hotmail.com>
2024-10-23 10:29:41 +0200

    cleanded upp scss file after implemented components

--- src/pages/registerpage/RegisterPage.tsx
+++ src/pages/registerpage/RegisterPage.tsx
@@ -1,6 +1,7 @@
 import { useRef, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import YellowBtn from '../../components/buttons/YellowBtn';
+import Rubrik from '../../components/rubrik/Rubrik';
 
 // import axios from 'axios';
 
@@ -52,7 +53,7 @@ const RegisterPage: React.FC = () => {
     <div className="container mt-5">
       <div className="row justify-content-center">
         <div className="col-12 text-center mb-4">
-          <h2 className="register-title">Bli medlem</h2>
+          <Rubrik title="Bli Medlem" />
         </div>
         <div className="col-md-6 col-lg-5">
           <div className="card shadow-sm">
