f06a9863b7855c248983583a63e9667d53b4fd06
asom1989 <asom.1989@yahoo.com>
2024-10-23 16:37:17 +0200

    created movie details bu needs some editing

--- src/pages/movieDetails/MovieDetailsPage.tsx
+++ src/pages/movieDetails/MovieDetailsPage.tsx
@@ -1,13 +1,33 @@
-import { Container, Row, Col, Card, Badge } from 'react-bootstrap';
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  ListGroup,
+  ButtonGroup,
+  Button,
+} from 'react-bootstrap';
 import NavButton from '../../components/header/NavButton';
+import { useState } from 'react';
+import PrimaryBtn from '../../components/buttons/PrimaryBtn';
 
 function MovieBooking() {
+  const [openCollapse, setOpenCollapse] = useState(false);
+  const [selectedOption, setSelectedOption] = useState<string>('1');
+  const fullText =
+    'Tonårige Marty McFly skickas av en slump tillbaka till år 1955 i en plutoniumdriven tidsmaskin, i form av en bil av märket DeLorean, som uppfunnits av hans vän, den smått galne vetenskapsmannen Doc Brown. Under resan tillbaka i tiden blir Marty tvungen att se till att hans tonåriga föräldrar träffas och blir förälskade i varandra - så att han själv kan återvända till framtiden för att ens existera alls!';
+
+  const shortText = fullText.substring(0, 200) + '...';
+  const handleSelect = (value: string) => {
+    setSelectedOption(value);
+    console.log('Selected:', value);
+  };
   return (
     <Container className="p-0">
       <Row className="mb-4 d-flex align-items-center justify-content-between g-3">
         {/* Poster Section */}
         <Col md={4}>
-          <Card>
+          <Card className="border border-0">
             <Card.Img
               className=" mx-auto "
               style={{ width: '80%', height: '45vh' }}
@@ -32,26 +52,47 @@ function MovieBooking() {
       {/* Date Buttons */}
       <Row className="mb-4">
         <Col className="d-flex justify-content-center flex-wrap gap-3">
-          <Badge pill bg="light" text="dark" className="px-4 py-3 mx-2">
-            Idag
-            <br />
-            24 sep
-          </Badge>
-          <Badge pill bg="light" text="dark" className="px-4 py-3 mx-2">
-            Onsdag
-            <br />
-            25 sep
-          </Badge>
-          <Badge pill bg="light" text="dark" className="px-4 py-3 mx-2">
-            Torsdag
-            <br />
-            26 sep
-          </Badge>
-          <Badge pill bg="light" text="dark" className="px-4 py-3 mx-2">
-            Fredag
-            <br />
-            27 sep
-          </Badge>
+          <div>
+            <h5>Selected Option: {selectedOption || 'None'}</h5>
+            <ButtonGroup className="bg-rosa p-3 mb-3 d-flex align-items-center justify-content-center flex-wrap gap-3">
+              <Button
+                className={`py-1 px-5 rounded border border-0 ${
+                  selectedOption === '1'
+                    ? 'body-bg-dark text-light'
+                    : 'bg-light text-dark'
+                }`}
+                onClick={() => handleSelect('1')}
+              >
+                Idag
+                <br />
+                24 sep
+              </Button>
+              <Button
+                className={`py-1 px-5 rounded border border-0 ${
+                  selectedOption === '2'
+                    ? 'body-bg-dark text-light'
+                    : 'bg-light text-dark'
+                }`}
+                onClick={() => handleSelect('2')}
+              >
+                Onsdag
+                <br />
+                25 sep
+              </Button>
+              <Button
+                className={`py-1 px-5 rounded border border-0 ${
+                  selectedOption === '3'
+                    ? 'body-bg-dark text-light'
+                    : 'bg-light text-dark'
+                }`}
+                onClick={() => handleSelect('3')}
+              >
+                Torsdag
+                <br />
+                26 sep
+              </Button>
+            </ButtonGroup>
+          </div>
         </Col>
       </Row>
 
@@ -63,46 +104,58 @@ function MovieBooking() {
       </Row>
 
       {/* Movie Details */}
-      <Row className="mb-4">
-        <Col md={6}>
-          <Card bg="dark" text="light" className="p-3 border-0">
-            <h4 className="my-3 text-center">Detaljer</h4>
-            <ul className="list-unstyled ">
-              <li className=" my-2 d-flex align-items-center justify-content-between flex-wrap">
-                <strong>Speltid:</strong> 116 minuter
-              </li>
-              <li className="my-2 d-flex align-items-center justify-content-between flex-wrap">
-                <strong>Genre:</strong> Science Fiction
-              </li>
-              <li className="my-2 d-flex align-items-center justify-content-between flex-wrap">
-                <strong>Regi:</strong> Robert Zemeckis
-              </li>
-              <li className="my-2 d-flex align-items-center justify-content-between flex-wrap">
-                <strong>Originaltitel:</strong> "Back to the Future"
-              </li>
-              <li className="my-2 d-flex align-items-center justify-content-between flex-wrap">
-                <strong>Skådespelare:</strong> Michael J. Fox, Christopher
-                Lloyd, Lea Thompson, Crispin Glover
-              </li>
-              <li className="my-2 d-flex align-items-center justify-content-between flex-wrap">
-                <strong>Inspelad:</strong> 1985
-              </li>
-            </ul>
+      <Row className="my-4 d-flex align-items-center justify-content-between g-3 ">
+        <Col md={4}>
+          <Card className="text-dark bg-rosa ">
+            <Card.Body>
+              <Card.Title className="my-2 text-center fw-bold">
+                Detaljer
+              </Card.Title>
+              <ListGroup variant="flush">
+                <ListGroup.Item className="text-dark px-0 bg-rosa  d-flex align-items-center justify-content-between flex-wrap">
+                  <strong>Speltid:</strong> <span>116 minuter</span>
+                </ListGroup.Item>
+                <ListGroup.Item className="text-dark px-0  bg-rosa  d-flex align-items-center justify-content-between flex-wrap">
+                  <strong>Genre:</strong> <span>Science Fiction</span>
+                </ListGroup.Item>
+                <ListGroup.Item className="text-dark px-0  bg-rosa d-flex align-items-center justify-content-between flex-wrap">
+                  <strong>Regi:</strong> <span>Robert Zemeckis</span>
+                </ListGroup.Item>
+                <ListGroup.Item className="text-dark px-0  bg-rosa d-flex align-items-center justify-content-between flex-wrap">
+                  <strong>Originaltitel:</strong>{' '}
+                  <span>"Back to the Future"</span>
+                </ListGroup.Item>
+                <ListGroup.Item className="text-dark px-0  bg-rosa d-flex align-items-center justify-content-between flex-wrap">
+                  <strong>Skådespelare:</strong>
+                  <span>
+                    Michael J. Fox, Christopher Lloyd, Lea Thompson, Crispin
+                    Glover
+                  </span>
+                </ListGroup.Item>
+                <ListGroup.Item className="text-dark px-0  bg-rosa d-flex align-items-center justify-content-between flex-wrap">
+                  <strong>Inspelad:</strong> <span>1985</span>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card.Body>
           </Card>
         </Col>
 
         {/* Movie Description */}
         <Col md={6}>
-          <Card bg="dark" text="light" className="p-3 border-0">
-            <Card.Text>
-              Tonårige Marty McFly skickas av en slump tillbaka till år 1955 i
-              en plutoniumdriven tidsmaskin, i form av en bil av märket
-              DeLorean, som uppfunnits av hans vän, den smått galne
-              vetenskapsmannen Doc Brown. Under resan tillbaka i tiden blir
-              Marty tvungen att se till att hans tonåriga föräldrar träffas och
-              blir förälskade i varandra - så att han själv kan återvända till
-              framtiden för att ens existera alls!
-            </Card.Text>
+          <Card className="p-4 border-0 bg-rosa text-dark fw-bold">
+            <Card.Text>{openCollapse ? fullText : shortText} </Card.Text>
+            <div
+              style={{ width: '100%' }}
+              className="d-flex justify-content-end"
+            >
+              <PrimaryBtn
+                title={openCollapse ? 'Visa Mindre' : 'Visa Mer'}
+                onClick={(e) => {
+                  e?.preventDefault();
+                  setOpenCollapse(!openCollapse);
+                }}
+              />
+            </div>
           </Card>
         </Col>
       </Row>
