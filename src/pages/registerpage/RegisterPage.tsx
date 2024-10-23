58dea4532469f2ed16bdfa9e40bca13d1fd4a4ff
Sara Johnsson <sara86persson@hotmail.com>
2024-10-23 18:06:25 +0200

    changed struktur on register page and color on placeholders

--- src/pages/registerpage/RegisterPage.tsx
+++ src/pages/registerpage/RegisterPage.tsx
@@ -46,111 +46,100 @@ const RegisterPage: React.FC = () => {
   const password = watch('user_password');
 
   return (
-    <div className="container mt-5">
-      <div className="row justify-content-center">
-        <div className="col-md-6 col-lg-5">
-          <div className="card shadow-sm">
-            <div className="card-body">
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="field-container">
-                  <label htmlFor="user_email" className="form-label">
-                    E-post
-                  </label>
-                  <input
-                    type="email"
-                    id="user_email"
-                    className="form-control"
-                    {...register('user_email', { required: 'E-post krävs' })}
-                    placeholder="Ange din e-post"
-                  />
-                  {errors.user_email && (
-                    <span>{errors.user_email.message}</span>
-                  )}
-                </div>
+    <section className="row justify-content-center">
+      <article className="col-md-6 col-lg-5 card shadow-sm">
+        <section className="card-body">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <fieldset className="field-container">
+              <label htmlFor="user_email" className="form-label">
+                E-post
+              </label>
+              <input
+                type="email"
+                id="user_email"
+                className="form-control"
+                {...register('user_email', { required: 'E-post krävs' })}
+                placeholder="Ange din e-post"
+              />
+              {errors.user_email && <span>{errors.user_email.message}</span>}
+            </fieldset>
 
-                <div className="field-container">
-                  <label htmlFor="user_password" className="form-label">
-                    Lösenord
-                  </label>
-                  <input
-                    type="password"
-                    id="user_password"
-                    className="form-control"
-                    {...register('user_password', {
-                      required: 'Lösenord krävs',
-                    })}
-                    placeholder="Ange ditt lösenord"
-                  />
-                  {errors.user_password && (
-                    <span>{errors.user_password.message}</span>
-                  )}
-                </div>
-
-                <div className="field-container">
-                  <label htmlFor="confirm_password" className="form-label">
-                    Repetera lösenord
-                  </label>
-                  <input
-                    type="password"
-                    id="confirm_password"
-                    className="form-control"
-                    {...register('confirm_password', {
-                      validate: (value) =>
-                        value === password || 'Lösenorden matchar inte',
-                    })}
-                    placeholder="Upprepa ditt lösenord"
-                  />
-                  {errors.confirm_password && (
-                    <span>{errors.confirm_password.message}</span>
-                  )}
-                </div>
-
-                <div className="field-container">
-                  <label htmlFor="first_name" className="form-label">
-                    Förnamn
-                  </label>
-                  <input
-                    type="text"
-                    id="first_name"
-                    className="form-control"
-                    {...register('first_name', { required: 'Förnamn krävs' })}
-                    placeholder="Ange ditt förnamn"
-                  />
-                  {errors.first_name && (
-                    <span>{errors.first_name.message}</span>
-                  )}
-                </div>
-
-                <div className="field-container">
-                  <label htmlFor="last_name" className="form-label">
-                    Efternamn
-                  </label>
-                  <input
-                    type="text"
-                    id="last_name"
-                    className="form-control"
-                    {...register('last_name', { required: 'Efternamn krävs' })}
-                    placeholder="Ange ditt efternamn"
-                  />
-                  {errors.last_name && <span>{errors.last_name.message}</span>}
-                </div>
-
-                <div className="button-group">
-                  <PrimaryBtn title="Avbryt" onClick={handleGoBack} />
-                  <PrimaryBtn title="Bli medlem" type="submit" />
-                </div>
-              </form>
-
-              {error && <div className="alert alert-danger mt-3">{error}</div>}
-
-              {success && (
-                <div className="alert alert-success mt-3">{success}</div>
+            <fieldset className="field-container">
+              <label htmlFor="user_password" className="form-label">
+                Lösenord
+              </label>
+              <input
+                type="password"
+                id="user_password"
+                className="form-control"
+                {...register('user_password', {
+                  required: 'Lösenord krävs',
+                })}
+                placeholder="Ange ditt lösenord"
+              />
+              {errors.user_password && (
+                <span>{errors.user_password.message}</span>
               )}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+            </fieldset>
+
+            <fieldset className="field-container">
+              <label htmlFor="confirm_password" className="form-label">
+                Repetera lösenord
+              </label>
+              <input
+                type="password"
+                id="confirm_password"
+                className="form-control"
+                {...register('confirm_password', {
+                  validate: (value) =>
+                    value === password || 'Lösenorden matchar inte',
+                })}
+                placeholder="Upprepa ditt lösenord"
+              />
+              {errors.confirm_password && (
+                <span>{errors.confirm_password.message}</span>
+              )}
+            </fieldset>
+
+            <fieldset className="field-container">
+              <label htmlFor="first_name" className="form-label">
+                Förnamn
+              </label>
+              <input
+                type="text"
+                id="first_name"
+                className="form-control"
+                {...register('first_name', { required: 'Förnamn krävs' })}
+                placeholder="Ange ditt förnamn"
+              />
+              {errors.first_name && <span>{errors.first_name.message}</span>}
+            </fieldset>
+
+            <fieldset className="field-container">
+              <label htmlFor="last_name" className="form-label">
+                Efternamn
+              </label>
+              <input
+                type="text"
+                id="last_name"
+                className="form-control"
+                {...register('last_name', { required: 'Efternamn krävs' })}
+                placeholder="Ange ditt efternamn"
+              />
+              {errors.last_name && <span>{errors.last_name.message}</span>}
+            </fieldset>
+
+            <section className="button-group">
+              <PrimaryBtn title="Avbryt" onClick={handleGoBack} />
+              <PrimaryBtn title="Bli medlem" type="submit" />
+            </section>
+          </form>
+
+          {error && <p className="alert alert-danger mt-3">{error}</p>}
+          {success && <p className="alert alert-success mt-3">{success}</p>}
+        </section>
+      </article>
+    </section>
   );
 };
 
