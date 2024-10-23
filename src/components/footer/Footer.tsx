a4c8b036db3928a482ab375c2d09786cec88fe83
EmiliaSundinAsp <emiliasundinasp@gmail.com>
2024-10-23 10:50:29 +0200

    create simple footer with headlines

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,3 +1,28 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './footer.scss';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="custom-footer text-white text-center py-3 mt-auto">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3">
+            <h5>Sociala medier</h5>
+          </div>
+          <div className="col-md-3">
+            <h5>Vi öppnar</h5>
+          </div>
+          <div className="col-md-3">
+            <h5>Adress</h5>
+          </div>
+          <div className="col-md-3">
+            <h5>Övrigt</h5>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
