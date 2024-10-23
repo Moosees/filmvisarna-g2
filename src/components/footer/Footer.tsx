696e0813c40d453b16ed3a0c0c768f9c6b6d9454
EmiliaSundinAsp <emiliasundinasp@gmail.com>
2024-10-23 22:12:11 +0200

    import fonts.scss and add text content in footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,23 +1,34 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './footer.scss';
+import '../../assets/sass/fonts.scss';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="custom-footer text-white text-center py-3 mt-auto">
+    <footer className="custom-footer text-center py-3 mt-auto">
       <div className="container">
         <div className="row">
           <div className="col-md-3">
-            <h5>Sociala medier</h5>
+            <h5>Sociala medier:</h5>
           </div>
           <div className="col-md-3">
-            <h5>Vi öppnar</h5>
+            <h5>Vi öppnar:</h5>
+            <p>Mån - fre 17:00</p>
+            <p>Lör - sön 13:00</p>
           </div>
           <div className="col-md-3">
-            <h5>Adress</h5>
+            <h5>Adress:</h5>
+            <p>Stockholmsvägen 11</p>
+            <p>193XX Norrtälje</p>
           </div>
           <div className="col-md-3">
-            <h5>Övrigt</h5>
+            <h5>Övrigt:</h5>
+            <p>Bli medlem</p>
+            <p>Biogodis</p>
+            <p>Teknik</p>
+            <p>Om oss</p>
+            <p>Avboka</p>
+            <p>Kontaka oss</p>
           </div>
         </div>
       </div>
