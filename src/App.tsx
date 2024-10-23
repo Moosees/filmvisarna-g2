4ab2532560c24f4b4baf22dd6e778c959ffb350e
EmiliaSundinAsp <emiliasundinasp@gmail.com>
2024-10-23 10:51:42 +0200

    render footer component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
           marginTop: '2rem',
         }}
       >
-        Footer
+        <Footer />
       </div>
     </div>
   );
