4ed5d6f187d39908065afc35c40a81c7d0740c50
EmiliaSundinAsp <emiliasundinasp@gmail.com>
2024-10-24 11:41:55 +0200

    add icons and remove bli medlem

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './footer.scss';
 import '../../assets/sass/fonts.scss';
+import { Instagram } from 'react-bootstrap-icons';
+import { TwitterX } from 'react-bootstrap-icons';
+import { Facebook } from 'react-bootstrap-icons';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="custom-footer text-center py-3 mt-auto">
+    <footer className="custom-footer text-center mt-auto">
       <div className="container">
         <div className="row">
           <div className="col-md-3">
             <h5>Sociala medier:</h5>
+            <Instagram size={35} />
+            <TwitterX size={35} />
+            <Facebook size={35} />
           </div>
           <div className="col-md-3">
             <h5>Vi öppnar:</h5>
@@ -23,7 +29,6 @@ const Footer: React.FC = () => {
           </div>
           <div className="col-md-3">
             <h5>Övrigt:</h5>
-            <p>Bli medlem</p>
             <p>Biogodis</p>
             <p>Teknik</p>
             <p>Om oss</p>
