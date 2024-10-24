f19d6b085e16990152341e5cc42c818d24b5d777
asom1989 <asom.1989@yahoo.com>
2024-10-24 17:20:52 +0200

    the MovieDetailsPage has been divided into multiple components, and logic has been added

---
+++ src/components/typography/TextBox.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import { Card, Col } from 'react-bootstrap';
+import PrimaryBtn from '../buttons/PrimaryBtn';
+
+interface TextBoxProps {
+  movieData: {
+    title: string;
+    movieInfo: {
+      description: string;
+    };
+  };
+}
+export default function TextBox({ movieData }: TextBoxProps) {
+  const [openCollapse, setOpenCollapse] = useState(false);
+
+  const fullText = movieData.movieInfo.description;
+  const shortText = fullText.substring(0, 150) + '...';
+  return (
+    <Col md={6} lg={6}>
+      <Card className="py-2 px-3 border-0 bg-rosa text-dark fw-bold">
+        <Card.Title className="my-2 fw-bold text-decoration-underline">
+          {movieData.title}
+        </Card.Title>
+
+        <Card.Text className="d-none d-xl-block custom-letterSpacing">
+          {fullText}
+        </Card.Text>
+
+        <Card.Text className="d-block d-xl-none custom-letterSpacing ">
+          {openCollapse ? fullText : shortText}{' '}
+        </Card.Text>
+        <div
+          style={{ width: '100%' }}
+          className="d-flex justify-content-end d-xl-none"
+        >
+          <PrimaryBtn
+            title={openCollapse ? 'Visa Mindre' : 'Visa Mer'}
+            onClick={(e) => {
+              e?.preventDefault();
+              setOpenCollapse(!openCollapse);
+            }}
+          />
+        </div>
+      </Card>
+    </Col>
+  );
+}
--- src/pages/movieDetails/MovieDetailsPage.tsx
+++ src/pages/movieDetails/MovieDetailsPage.tsx
@@ -1,16 +1,13 @@
-import {
-  Container,
-  Row,
-  Col,
-  Card,
-  ListGroup,
-  ButtonGroup,
-  Button,
-} from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import NavButton from '../../components/header/NavButton';
 import { useState } from 'react';
-import PrimaryBtn from '../../components/buttons/PrimaryBtn';
 import MainHeading from '../../components/mainHeading/MainHeading';
+import MovieTrailer from '../../components/poster/MovieTrailer';
+import MoviePoster from '../../components/poster/MoviePoster';
+import ScreeningSelect from '../../components/buttons/ScreeningSelect';
+import TextTable from '../../components/typography/TextTable';
+import TextBox from '../../components/typography/TextBox';
+import PrimaryBtn from '../../components/buttons/PrimaryBtn';
 
 const movieData = {
   id: 5,
@@ -64,18 +61,11 @@ const movieData = {
 };
 
 function MovieBooking() {
-  const [openCollapse, setOpenCollapse] = useState(false);
   const [selectedScreening, setSelectedScreening] = useState<number>(
     movieData.screeningDetails[0].screeningId
   );
-  const fullText = movieData.movieInfo.description;
+  const [openTrailer, setOpenTrailer] = useState(false);
 
-  const shortText = fullText.substring(0, 150) + '...';
-
-  const handleSelect = (value: number) => {
-    setSelectedScreening(value);
-    console.log('Selected:', value);
-  };
   return (
     <>
       <Container className="p-0">
@@ -85,57 +75,35 @@ function MovieBooking() {
         {/* Row for Poster and Video */}
         <Row className=" my-3 d-flex align-items-center justify-content-evenly ">
           {/* Poster Section */}
-          <Col md={4} className="d-none d-md-flex">
-            <Card className="border border-0">
-              <Card.Img
-                className=" mx-auto "
-                style={{ width: '80%', height: '38vh' }}
-                src={movieData.posterUrl}
-                alt={movieData.title}
-              />
-            </Card>
-          </Col>
-
+          {openTrailer ? (
+            <MovieTrailer movieData={movieData} />
+          ) : (
+            <MoviePoster movieData={movieData} />
+          )}
+          {/*  */}
           {/* Video Section */}
-          <Col md={6}>
-            <iframe
-              style={{ width: '100%', height: '38vh' }}
-              src={movieData.movieInfo.trailer}
-              title={`${movieData.title} trailer`}
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
+          {/* <MovieTrailer movieData={movieData} /> */}
+          <div
+            style={{ width: '100%' }}
+            className="d-flex justify-content-center "
+          >
+            <PrimaryBtn
+              title={openTrailer ? 'Visa Poster' : 'Visa Trailer'}
+              onClick={(e) => {
+                e?.preventDefault();
+                setOpenTrailer(!openTrailer);
+              }}
             />
-          </Col>
+          </div>
         </Row>
-
         {/* Date Buttons */}
         <Row className="mb-3">
-          <Col className="d-flex justify-content-center flex-wrap gap-3">
-            <div>
-              {/* <h5>Screening Id: {selectedOption || 'None'}</h5> */}
-              <ButtonGroup className="bg-rosa p-2 d-flex align-items-center justify-content-center flex-wrap gap-2 ">
-                {movieData.screeningDetails.map((screening) => (
-                  <Button
-                    key={screening.screeningId}
-                    style={{ fontSize: '.7em' }}
-                    className={`py-1 px-3  rounded border border-0 text-capitalize ${
-                      selectedScreening === screening.screeningId
-                        ? 'body-bg-dark text-light'
-                        : 'bg-light text-dark'
-                    }`}
-                    onClick={() => handleSelect(screening.screeningId)}
-                  >
-                    {screening.dayName}
-                    <br />
-                    {screening.screeningDate} <br />
-                    <span>{screening.timeRange}</span>
-                  </Button>
-                ))}
-              </ButtonGroup>
-            </div>
-          </Col>
+          <ScreeningSelect
+            selectedScreening={selectedScreening}
+            setSelectedScreening={setSelectedScreening}
+            movieData={movieData}
+          />
         </Row>
-
         {/* Book Tickets Button */}
         <Row className="mb-4">
           <Col className="text-center">
@@ -145,73 +113,11 @@ function MovieBooking() {
             />
           </Col>
         </Row>
-
         {/* Movie Details and  Movie Description  */}
         <Row className="my-3 d-flex align-items-center justify-content-between g-3 ">
-          <Col md={6} xl={4}>
-            <Card className="text-dark bg-rosa ">
-              <Card.Body className="py-2">
-                <Card.Title className="my-2 text-center fw-bold text-decoration-underline">
-                  Detaljer
-                </Card.Title>
-                <ListGroup variant="flush">
-                  <ListGroup.Item className="text-dark p-1 px-0 bg-rosa  d-flex align-items-center justify-content-between flex-wrap">
-                    <strong>Speltid:</strong>{' '}
-                    <span>{movieData.playTime} Minuter</span>
-                  </ListGroup.Item>
-                  <ListGroup.Item className="text-dark p-1 px-0  bg-rosa  d-flex align-items-center justify-content-between flex-wrap">
-                    <strong>Genre:</strong>{' '}
-                    <span>{movieData.genres.join(', ')}</span>
-                  </ListGroup.Item>
-                  <ListGroup.Item className="text-dark p-1 px-0  bg-rosa d-flex align-items-center justify-content-between flex-wrap">
-                    <strong>Regi:</strong>{' '}
-                    <span>{movieData.movieInfo.director}</span>
-                  </ListGroup.Item>
-                  {/* <ListGroup.Item className="text-dark p-1 px-0  bg-rosa d-flex align-items-center justify-content-between flex-wrap">
-                    <strong>Originaltitel:</strong>{' '}
-                    <span>"Back to the Future"</span>
-                  </ListGroup.Item> */}
-                  <ListGroup.Item className="text-dark p-1 px-0  bg-rosa d-flex align-items-center justify-content-between flex-wrap">
-                    <strong>Skådespelare:</strong>
-                    <span>{movieData.movieInfo.actors.join(', ')}</span>
-                  </ListGroup.Item>
-                  <ListGroup.Item className="text-dark p-1 px-0  bg-rosa d-flex align-items-center justify-content-between flex-wrap">
-                    <strong>Inspelad:</strong>{' '}
-                    <span>{movieData.movieInfo.year_recorded}</span>
-                  </ListGroup.Item>
-                </ListGroup>
-              </Card.Body>
-            </Card>
-          </Col>
-
+          <TextTable movieData={movieData} />
           {/* Movie Description */}
-          <Col md={6} lg={6}>
-            <Card className="py-2 px-3 border-0 bg-rosa text-dark fw-bold">
-              <Card.Title className="my-2 fw-bold text-decoration-underline">
-                {movieData.title}
-              </Card.Title>
-
-              <Card.Text className="d-none d-xl-block custom-letterSpacing">
-                {fullText}
-              </Card.Text>
-
-              <Card.Text className="d-block d-xl-none custom-letterSpacing ">
-                {openCollapse ? fullText : shortText}{' '}
-              </Card.Text>
-              <div
-                style={{ width: '100%' }}
-                className="d-flex justify-content-end d-xl-none"
-              >
-                <PrimaryBtn
-                  title={openCollapse ? 'Visa Mindre' : 'Visa Mer'}
-                  onClick={(e) => {
-                    e?.preventDefault();
-                    setOpenCollapse(!openCollapse);
-                  }}
-                />
-              </div>
-            </Card>
-          </Col>
+          <TextBox movieData={movieData} />
         </Row>
       </Container>
     </>
