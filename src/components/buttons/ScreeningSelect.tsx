79a5fbeda2bb846d5f479622c464c8a6a252ad82
asom1989 <asom.1989@yahoo.com>
2024-11-03 13:52:18 +0100

    add hover effect on  ScreeningSelect buttons

--- src/components/buttons/ScreeningSelect.tsx
+++ src/components/buttons/ScreeningSelect.tsx
@@ -25,12 +25,12 @@ export default function ScreeningSelect({
     <Col className="d-flex justify-content-center flex-wrap gap-3">
       <div>
         {/* <h5>Screening Id: {selectedScreening || 'None'}</h5> */}
-        <ButtonGroup className="bg-rosa p-2 d-flex align-items-center justify-content-center flex-wrap gap-2 ">
+        <ButtonGroup className="bg-rosa p-2 d-flex align-items-center justify-content-center flex-wrap gap-1 ">
           {movieData.screeningDetails.map((screening) => (
             <Button
               key={screening.screeningId}
               style={{ fontSize: '.7em' }}
-              className={`py-1 px-3 rounded border-0  ${
+              className={`py-1 px-3 rounded  btn-custom:hover border-btn ${
                 selectedScreening === screening.screeningId
                   ? 'body-bg-dark text-light'
                   : 'bg-light text-dark'
