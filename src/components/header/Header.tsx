7bc860611b6bbabf290f2fdac9276f73989560c3
asom1989 <asom.1989@yahoo.com>
2024-11-09 17:55:33 +0100

    add hover to Medlem meny

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -31,7 +31,7 @@ function Header() {
         {/* <NavButton to="/evenemang" label="Evenemang" /> */}
         <Dropdown>
           <Dropdown.Toggle bsPrefix="custom-toggle" id="medlem">
-            <img src={User} alt="Medlem meny" className="img-fluid" />
+            <img src={User} alt="Medlem meny" className="img-fluid scale" />
           </Dropdown.Toggle>
           <Dropdown.Menu className="dropdown-menu shadow">
             {!isLoggedIn && ( // Render these items only when not logged in
