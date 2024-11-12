6362bab33161bf9599e8e43d1e5af082e14ed6b1
EmiliaSundinAsp <emiliasundinasp@gmail.com>
2024-11-12 23:27:29 +0100

    change text positin and add more contact information to about-page

--- src/pages/aboutPage/AboutPage.tsx
+++ src/pages/aboutPage/AboutPage.tsx
@@ -4,15 +4,15 @@ import { Container, Row, Col, Form, Alert } from 'react-bootstrap';
 function AboutPage() {
   return (
     <Container className="d-flex flex-column align-items-center justify-content-center">
-      <Row className="w-100">
+      <Row className="w-100 mb-5">
         <Col
           xs={12}
           className="rounded bg-rosa mt-3 p-3 justify-content-center"
         >
-          <h4 className="text-black text-center">
+          <h4 className="text-black">
             Filmvisarna – Där 80-talet lever vidare, en film i taget!
           </h4>
-          <p className="text-black text-center">
+          <p className="text-black">
             <br />
             Filmvisarna är en nyöppnad biograf med två moderna och bekväma
             salonger. <br />
@@ -29,27 +29,26 @@ function AboutPage() {
         </Col>
       </Row>
       <div className="row justify-content-center">
-        <h1 className="bg-rosa text-dark text-center fs-5 fw-bold text-decoration-underline py-2 px-5 custom-rounded col-auto">
+        <h1 className="bg-rosa text-dark fs-5 fw-bold text-decoration-underline py-2 px-5 custom-rounded col-auto">
           Kontakta oss
         </h1>
       </div>
-      <Row className="w-100">
+      <Row className="w-100 mt-4">
         <Col
           xs={12}
           className="rounded bg-rosa mt-3 p-3 justify-content-center"
         >
-          <h4 className="text-black text-center">Kontakta oss:</h4>
-          <p className="text-black text-center">
+          <h4 className="text-black">Kontakta oss:</h4>
+          <p className="text-black">
             <br />
-            Telefon: <br />
-            E-post: <br />
-            Atmosfären präglas av neonljus, färgstarka affischer och autentiska
-            detaljer från 80-talets stora filmverk.
-            <br /> Filmvisarna är en perfekt plats för att återuppleva
-            actionfilmer, romantiska komedier och kultklassiker från science
-            fiction-genren. <br />
+            Telefon: 073 - XXX XX XX
             <br />
-            Välkomna tillbaka till 80-talet!
+            E-post: filmvisarna@gmail.com
+            <br />
+            <br />
+            Vi öppnar: <br />
+            Mån - fre: 17:00 <br />
+            Lör - sön: 13:00 <br />
           </p>
         </Col>
       </Row>
