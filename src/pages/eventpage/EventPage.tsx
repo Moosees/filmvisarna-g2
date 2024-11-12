e1baee0becfe2922277f4ccd7ecfedf8ce2d7122
Sara Johnsson <sara86persson@hotmail.com>
2024-11-12 11:10:15 +0100

    styling fix event page

--- src/pages/eventpage/EventPage.tsx
+++ src/pages/eventpage/EventPage.tsx
@@ -11,20 +11,24 @@ const EventPage: React.FC = () => {
         <Col xs={12} className="rounded bg-rosa mt-3 p-3">
           <h4 className="text-black">Astrid Lindgrens Matinéhelg i December</h4>
           <p className="text-black">
-            Filmer som visas lördag 14 december: <br />
+            <span className="fw-bold">
+              Filmer som visas lördag 14 december:
+            </span>{' '}
+            <br />
             <br /> 1. Alla barnen i Bullerbyn <br /> Tid: kl. 13:00 (stora
             salongen) <br />
             <br /> 2. Mio min Mio <br /> Tid: kl. 15:30 (stora salongen) <br />
-            <br /> <span className="">söndag 15 december:</span> <br />
+            <br /> <span className="fw-bold">söndag 15 december:</span> <br />
             <br /> 3. Ronja Rövardotter <br /> Tid: kl. 13:00 (stora salongen){' '}
             <br />
-            <br /> Förmåner: Halva priset på popcorn: De som bokar biljetter
-            till alla tre filmer får 50 % rabatt på popcorn vid varje visning.{' '}
+            <br /> <span className="fw-bold">Förmåner: </span> Halva priset på
+            popcorn: De som bokar biljetter till alla tre filmer får 50 % rabatt
+            på popcorn vid varje visning. <br />
+            <br /> <span className="fw-bold">Familjerabatt:</span>{' '}
+            Specialerbjudande för familjer som bokar fler än fyra biljetter.{' '}
             <br />
-            <br /> Familjerabatt: Specialerbjudande för familjer som bokar fler
-            än fyra biljetter. <br />
-            <br /> Bokningsinformation: Biljetter kan bokas via hemsidan eller i
-            biljettkassan. <br />
+            <br /> <span className="fw-bold">Bokningsinformation:</span>{' '}
+            Biljetter kan bokas via hemsidan eller i biljettkassan. <br />
             Popcornrabatten gäller för de som bokar alla tre filmer. <br />
             <br /> En underbar matinéhelg med några av Astrid Lindgrens mest
             älskade berättelser!
@@ -35,22 +39,27 @@ const EventPage: React.FC = () => {
 
         <Col xs={12} className="rounded bg-rosa mt-3 p-3">
           <h4 className="text-black">
-            Välkommen till en ikonisk skräck kväll hos Filmvisarna!
+            Upplev en ikonisk skräckfilmskväll hos Filmvisarna!
           </h4>
           <p className="text-black">
-            Filmer som visas fredag 20:e December: <br />
-            <br /> 1. Nightmare on Elm Street <br /> Tid: kl. 20:00 (stora
+            <span className="fw-bold">
+              Filmer som visas fredag 20:e December:
+            </span>{' '}
+            <br />
+            <br /> 1. Nightmare on Elm Street <br /> Tid: kl. 19:30 (stora
             salongen) <br />
             <br /> 2. The Shining
             <br />
             Tid: kl. 21:30 (stora salongen) <br />
             <br /> 3. Friday the 13th <br />
-            Tid: kl. 17:30 (lilla salongen) <br />
-            <br /> 4. Poltergeist <br /> Tid: kl. 19:30 (lilla salongen) <br />
-            <br /> Kom utklädd och vi bjuder på en liten popcorn! <br />
+            Tid: kl. 19:30 (lilla salongen) <br />
+            <br /> 4. Poltergeist <br /> Tid: kl. 21:30 (lilla salongen) <br />
             <br />
-            Bokningsinformation: Biljetter kan bokas via hemsidan eller i
-            biljettkassan.
+            <span className="fw-bold">Förmåner: </span> Kom utklädd och vi
+            bjuder på en liten popcorn! <br />
+            <br />
+            <span className="fw-bold">Bokningsinformation:</span> Biljetter kan
+            bokas via hemsidan eller i biljettkassan.
           </p>
         </Col>
       </Row>
