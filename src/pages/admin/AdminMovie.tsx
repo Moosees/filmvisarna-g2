66d2537ad088f4f8cad7d73f5262e797874b06dd
Linus Almgren <linus.almgren@gmail.com>
2024-11-12 04:55:56 +0100

    add AdminMovie fields for non json movie table columns

--- src/pages/admin/AdminMovie.tsx
+++ src/pages/admin/AdminMovie.tsx
@@ -37,7 +37,7 @@ function AdminMovieEdit() {
             <Form.Label className="form-label">Filmtitel</Form.Label>
             <Form.Control
               defaultValue={movieData?.title}
-              type="title"
+              type="text"
               className="form-control-field"
               placeholder="Primär titel"
               {...register('title', { required: 'Titeln är obligatorisk' })}
@@ -45,6 +45,69 @@ function AdminMovieEdit() {
             />
             <Form.Control.Feedback type="invalid">
               {errors.title?.message}
+            </Form.Control.Feedback>
+          </Form.Group>
+          {/* <Form.Group controlId="altTitle" className="field-container mb-3"> */}
+          {/*   <Form.Label className="form-label">Alternativ titel</Form.Label> */}
+          {/*   <Form.Control */}
+          {/*     defaultValue={movieData?.altTitle} */}
+          {/*     type="text" */}
+          {/*     className="form-control-field" */}
+          {/*     placeholder="Alternativ/original titel" */}
+          {/* {...register('altTitle', { required: false })} */}
+          {/*     isInvalid={!!errors.altTitle} */}
+          {/*   /> */}
+          {/*   <Form.Control.Feedback type="invalid"> */}
+          {/*     {errors.altTitle?.message} */}
+          {/*   </Form.Control.Feedback> */}
+          {/* </Form.Group> */}
+          <Form.Group controlId="playTime" className="field-container mb-3">
+            <Form.Label className="form-label">Speltid</Form.Label>
+            <Form.Control
+              defaultValue={movieData?.playTime}
+              type="number"
+              className="form-control-field"
+              placeholder="Tid i minuter"
+              {...register('playTime', {
+                required: 'Speltid är obligatorisk',
+                min: 0,
+                max: 999,
+              })}
+              isInvalid={!!errors.playTime}
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.playTime?.message}
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group controlId="age" className="field-container mb-3">
+            <Form.Label className="form-label">Åldersgräns</Form.Label>
+            <Form.Select
+              defaultValue={movieData?.age || 11}
+              className="form-control-field"
+              {...register('age', { required: 'Ålder är obligatorisk' })}
+              isInvalid={!!errors.age}
+            >
+              <option value={1}>Barntillåten</option>
+              <option value={7}>Från 7 år</option>
+              <option value={11}>Från 11 år</option>
+              <option value={15}>Från 15 år</option>
+            </Form.Select>
+            <Form.Control.Feedback type="invalid">
+              {errors.age?.message}
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group controlId="posterUrl" className="field-container mb-3">
+            <Form.Label className="form-label">Filmposter</Form.Label>
+            <Form.Control
+              defaultValue={movieData?.posterUrl}
+              type="text"
+              className="form-control-field"
+              placeholder="Url till poster"
+              {...register('posterUrl', { required: 'Poster är obligatorisk' })}
+              isInvalid={!!errors.posterUrl}
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.posterUrl?.message}
             </Form.Control.Feedback>
           </Form.Group>
         </div>
