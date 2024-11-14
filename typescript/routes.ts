01dfaf454b30c2ef62536c9639df3f9481d852ee
Linus Almgren <linus.almgren@gmail.com>
2024-11-14 03:43:47 +0100

    clean up routes.ts a bit

--- typescript/routes.ts
+++ typescript/routes.ts
@@ -1,19 +1,16 @@
 import express from 'express';
+import eventController from './controller/eventController.js';
 import moviesController from './controller/moviesController.js';
 import reservationsController from './controller/reservationsController.js';
 import seatsController from './controller/seatsController.js';
 import ticketsController from './controller/ticketsController.js';
-import { isAdmin, isAuthenticated } from './middleware/authMiddleware.js';
-// import { isAdmin } from './middleware/authMiddleware.js';
 import usersController from './controller/usersController.js';
-// import Mailer from './helpers/nodemailer.js';
 import Mailer from './helpers/nodemailer.js';
-import eventController from './controller/eventController.js';
+import { isAdmin, isAuthenticated } from './middleware/authMiddleware.js';
 
 const router = express.Router();
 
-// NOTE: use socket.io or server sent events for handling this
-// get what seats are reserved by other people
+// get what seats are reserved by other people NOTE: not needed?
 router.get('/reservedSeats/:screening_id', seatsController.getReservedSeats);
 // get seats not reserved by other people
 router.get(
@@ -41,28 +38,21 @@ router.put('/reservation', reservationsController.changeReservation);
 // cancel a reservation
 router.delete('/reservation', reservationsController.cancelReservation);
 
-/**-----------------------------------------------
- * @desc    movies routes
- ------------------------------------------------*/
-//--------------- Start movies routes ----------------------
-
+//--------------- Movie routes ----------------------
 // find movie(s) by filtering (age, date) (use req.query for filtering or split into separate routes?)
-// this is an example explaining how the filter works:
-//http://localhost:3002/movie?age=15&date=2024-11-04
 router.get('/movie', moviesController.filterMovies);
 // Get movies to be shown today
 router.get('/todaysMovies', moviesController.getTodaysMovie);
 // Show movie details and screening times
 router.get('/movie/:id', moviesController.getMovie);
 
-//---------------- Admin routes
+//--------------- Admin routes ----------------------
 router.post('/movie', isAdmin, moviesController.addMovie);
 router.get('/movies', isAdmin, moviesController.getAllMovies);
 router.put('/movie/:id', isAdmin, moviesController.updateMovie);
 router.delete('/movie/:id', isAdmin, moviesController.deleteMovie);
 
-//--------------- end movies routes ----------------------
-
+//--------------- User routes ----------------------
 // register a member - body: {email, password, firstName, lastName}
 router.post('/user/register', usersController.register);
 
@@ -101,6 +91,7 @@ router.patch('/user', usersController.updateUserDetails);
 
 router.get('/ping', usersController.ping);
 
+//--------------- Event routes ----------------------
 router.get('/event/scary-movies', eventController.getAllScaryMovies);
 
 router.get(
@@ -108,16 +99,9 @@ router.get(
   eventController.getAllAstridLindgrenMovies
 );
 
+// NOTE: not needed?
 router.post('/send-email', (req, res) => {
   const { to, subject, text, html, attachments } = req.body;
-
-  console.log('Sending email with details:', {
-    to,
-    subject,
-    text,
-    html,
-    attachments,
-  });
 
   try {
     Mailer.send(to, subject, text, html, attachments);
