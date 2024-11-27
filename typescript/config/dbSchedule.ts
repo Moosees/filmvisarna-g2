a8ae7b176309e9b1696751afa090d829c4f7e9ad
Linus Almgren <linus.almgren@gmail.com>
2024-11-27 21:28:41 +0100

    add function to update screening start time

--- typescript/config/dbSchedule.ts
+++ typescript/config/dbSchedule.ts
@@ -1,5 +1,17 @@
 import schedule from 'node-schedule';
 import sendEmail from '../utils/sendEmail.js';
+import db from './connectDB.js';
+
+const addTimeToScreenings = async () => {
+  const query = `
+    UPDATE screening
+    SET start_time = DATE_ADD(start_time, INTERVAL 7 DAY) 
+    WHERE start_time < CURRENT_DATE(); 
+  `;
+
+  const [queryResult] = await db.query(query);
+  console.log(queryResult);
+};
 
 process.on('SIGINT', async () => {
   console.log('gracefully shutting down node-schedule');
@@ -8,8 +20,10 @@ process.on('SIGINT', async () => {
 });
 
 export const scheduleMovieMover = () => {
-  const job = schedule.scheduleJob('0 30 1 * * *', () => {
-    console.log('running job', new Date());
+  const job = schedule.scheduleJob('0 30 1 * * *', async () => {
+    console.log('running job at', new Date());
+    await addTimeToScreenings();
+
     sendEmail(
       'filmvisarnabio@gmail.com',
       'Visningarna uppdateras',
